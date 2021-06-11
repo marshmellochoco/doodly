836d3a914cce468ac7100ba7f0ad1f798fe267b0
Ng Jing Kai <37670295+marshmellochoco@users.noreply.github.com>
2021-06-12 02:57:24 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteMetadata: {
+    title: "doodly",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "vrsByWI23wo8tBOESv5YGdpEuDRpLZLMO60dLch9CN8",
+        spaceId: "e07ev7fdcztb",
+      },
+    },
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
